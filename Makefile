7752f285de9b94e97ea11907b9036187a3c1c4c0
vlad <mlprimitif@gmail.com>
2025-04-09 21:36:10 +0700

    make a connection into db

--- Makefile
+++ Makefile
@@ -1,4 +1,28 @@
 PATH_FILE = ./cmd/api/
 
+POSTGRES_PASSWORD = 123456
+POSTGRES_USER = postgres
+DOCKER_CONTAINER_NAME = postgres-db
+DB_PORT = 5432
+DB_NAME = greenlight
+DB_USERNAME = greenlight
+
+
+start-postgres:
+	docker run --name $(DOCKER_CONTAINER_NAME) \
+	-e POSTGRES_USER=$(POSTGRES_USER) \
+	-e POSTGRES_PASSWORD=$(POSTGRES_PASSWORD) \
+	-p $(DB_PORT):5432 \
+	-d postgres:latest
+
+login-postgres:
+	docker exec -it $(DOCKER_CONTAINER_NAME) \
+	psql -U $(POSTGRES_USER) \
+
+login-db:
+	docker exec -it $(DOCKER_CONTAINER_NAME) \
+	psql -U $(DB_USERNAME) \
+	-d $(DB_NAME)
+
 run:
 	go run $(PATH_FILE)
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -9,17 +11,21 @@ import (
 	"time"
 
 	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
 )
 
 const version string = "1.0.0"
 
-type config struct  {
+type config struct {
 	port int
-	env string
+	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
-	cfg config
+	cfg    config
 	logger *log.Logger
 }
 
@@ -28,28 +34,57 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 8000, "Api server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:123456@localhost:5432/greenlight?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
 
 	// Init logger
-	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Init database
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	logger.Printf("database connection pool is established")
 
 	var app *application = &application{
-		cfg: cfg,
+		cfg:    cfg,
 		logger: logger,
 	}
 
 	var router *httprouter.Router = app.routes()
-	
 
-  serve := http.Server{
-		Addr: fmt.Sprintf(":%d", app.cfg.port),
-		Handler: router,
-		IdleTimeout: time.Minute,
-		ReadTimeout: 10 * time.Second,
+	serve := http.Server{
+		Addr:         fmt.Sprintf(":%d", app.cfg.port),
+		Handler:      router,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 	logger.Printf("Starting server in %s mode on port %d", app.cfg.env, app.cfg.port)
-	err := serve.ListenAndServe()
+	err = serve.ListenAndServe()
 	logger.Fatal(err)
 
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	// make a connection
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// validate the connection, if the connection cannot be mad in 5 seconds, return an error
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel() // executes after the function return
+
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
