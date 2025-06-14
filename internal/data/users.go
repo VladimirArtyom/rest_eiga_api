855d4433ec834785c3ffca4cbc9c86f0987d187a
vlad <mlprimitif@gmail.com>
2025-06-14 19:35:05 +0900

    ajouter nouvelles implementations de base de donnees de utilisateurs

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/VladimirArtyom/rest_eiga_api/internal/validator"
+	"github.com/lib/pq"
 )
 
 type User struct {
@@ -14,6 +18,112 @@ type User struct {
 	Password password `json:"-"`
 	Activated bool `json:"activated"`
 	Version int `json:"-"`
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+var ErrDuplicateEmail = errors.New("duplicate email")
+
+func (u *UserModel) Insert(user *User) error {
+
+	query := `
+		INSERT INTO users (name, email, password_hash, activated)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version
+	`
+	args := []interface{}{
+	  user.Name,
+		user.Email,
+		user.Password.hash,
+		user.Activated,
+	} 
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := u.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
+	if err != nil {
+		pqErr, ok := err.(*pq.Error); if ok {
+			switch {
+			case pqErr.Code.Name() == "unique_violation" && pqErr.Constraint == "users_email_key":
+				return ErrDuplicateEmail
+			}
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (u *UserModel)_GetByEmail(email string) (*User, error ) {
+	
+	query := `
+		SELECT id, created_at, name, email, password_hash, activated, version
+		FROM users
+		WHERE email = $1
+	`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var user User
+	err := u.DB.QueryRowContext(ctx, query, email).Scan(
+		&user.ID, &user.CreatedAt,
+		&user.Name, &user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
+}
+
+
+func (u *UserModel) Update(user *User) error {
+
+	query := `
+		UPDATE users
+		SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1
+		WHERE id = $5 AND version = $6 
+		RETURNING version
+	`
+
+	args := []interface{}{
+		user.Name,
+		user.Email,
+		user.Password.hash,
+		user.Activated,
+		user.ID,
+		user.Version,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := u.DB.QueryRowContext(ctx, query, args...).Scan(&user.Version)
+	if err != nil {
+		pqErr, ok := err.(*pq.Error); if ok {
+			switch {
+				case pqErr.Code.Name() == "unique_violation" && pqErr.Constraint == "users_email_key":
+					return ErrDuplicateEmail
+				case errors.Is(err, sql.ErrNoRows):
+					return ErrEditConflict
+			}
+		}
+
+		return err
+	}
+
+	return nil
 }
 
 
@@ -31,7 +141,6 @@ func ValidatePasswordPlainText(v *validator.Validator, password string) {
 	v.Check(len(password) <= 72, "password", "must be at most 72 bytes long")
 	
 }
-
 
 func ValidateUser(v *validator.Validator, user *User) {
 	
